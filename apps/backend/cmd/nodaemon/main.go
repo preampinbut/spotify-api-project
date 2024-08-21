8bdb8b930e244dca66e03e424b9dd29894a61921
Pream Pinbut <momozahara@hotmail.com>
2024-08-21 08:04:23 +0700

    chore(backend)

--- apps/backend/cmd/nodaemon/main.go
+++ apps/backend/cmd/nodaemon/main.go
@@ -1,9 +1,3 @@
-// This example demonstrates how to authenticate with Spotify using the authorization code flow with PKCE.
-// In order to run this example yourself, you'll need to:
-//
-//  1. Register an application at: https://developer.spotify.com/my-applications/
-//     - Use "http://localhost:8080/callback" as the redirect URI
-//  2. Set the SPOTIFY_ID environment variable to the client ID you got in step 1.
 package main
 
 import (
